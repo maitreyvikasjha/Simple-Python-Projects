ecf1f6b6d5664794a20178fed5a79369f1638d04
Maitrey Vikas Jha <maitreyvikasjha@gmail.com>
2019-04-30 00:29:35 +0530

    Hello World Commit

--- Input.py
+++ Input.py
@@ -1,1 +1,4 @@
-input()
+#Program to print user input
+
+name = input('What is your name buddy? ')
+print('Hi '+ name )
---
+++ Variables.py
@@ -0,0 +1,19 @@
+#Integers
+price1 = 100
+price2 = 20
+
+
+Print = price1+price2
+print(Print)
+
+
+#Floats
+
+rating = 4.574
+print(rating)
+
+#Booleans
+is_published = False
+
+
+
