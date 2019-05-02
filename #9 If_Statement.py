92977a212536941f08a1cdcb6a8faddd18f59cf0
Maitrey Vikas Jha <maitreyvikasjha@gmail.com>
2019-05-02 23:07:44 +0530

    Formatting Commit

---
+++ #9 If_Statement.py
@@ -0,0 +1,13 @@
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("It's a hot day")
+    print("Stay hydrated")
+
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+
+else:
+    print("Have a nice day")
