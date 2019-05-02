45453c713a0967aba1b4a5beae2c391b1281fe7a
Maitrey Vikas Jha <maitreyvikasjha@gmail.com>
2019-05-02 22:36:07 +0530

    Formatting Commit

---
+++ #8 Artimetic_Operations.py
@@ -0,0 +1,14 @@
+x = input("Enter the Value for X: ")
+y = input("Enter the Value for Y: ")
+
+print("The addition of X and Y is :")
+print(int(x) + int(y))
+
+print("The subtracion of X and Y is :")
+print(int(x) - int(y))
+
+print("The mutltiplication of X and Y is :")
+print(int(x) * int(y))
+
+print("The division of X and Y is :")
+print(int(x) / int(y))
